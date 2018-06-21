6990ba61099d2329ac822350cec1324ea7ac485d
Yura <ignatyug@gmail.com>
2018-06-21 23:54:34 +0300

    Printer

---
+++ src/main/java/com/yurgen/calculator/Printer.java
@@ -0,0 +1,11 @@
+package com.yurgen.calculator;
+
+public class Printer {
+
+
+
+    void print(int left, int right, char operation, double result) {
+        System.out.println(left + " " + operation);
+    }
+
+}
