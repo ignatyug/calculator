d0c53db4b2cbac46de619604ed14cb5b89b6bbc5
Yura <ignatyug@gmail.com>
2018-06-26 00:03:32 +0300

    Task2

--- src/main/java/com/yurgen/mas/TaskMas2.java
+++ src/main/java/com/yurgen/mas/TaskMas2.java
@@ -5,17 +5,22 @@ import java.util.Scanner;
 public class TaskMas2 {
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int[] mas = {2, 4, 6, 8, 9,};
-        System.out.print("Укажите любое число: ");
+        /*Scanner in = new Scanner(System.in);
+        System.out.print("Введите число: ");
+        int a = in.nextInt();
+        int[] mas = {2, 4, 6, 8, 9};
+        boolean found = false;
         for (int i = 0; i < mas.length; i++) {
-            mas[i] = in.nextInt();
-            if (mas[i] <= mas.length) {
-                System.out.println("Вы ввели число которое соответствуе заданому параметру: ");
-            } else {
-                System.out.println("Вы ввели число котрое не соответствует не одному параметру: ");
+            if (mas[i] == a) {
+                System.out.println("Вы ввели число которое соответствуе заданому параметру: " + i);
+                found = true;
             }
         }
+        if (found == false) {
+            System.out.println("не найден");
+        }*/
+        char c = 'a';
+        System.out.println((int)c);
     }
 }
 
