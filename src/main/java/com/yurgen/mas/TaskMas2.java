470d163bd590063aafe9f741dbc233065ea2f57f
ignat <ignatyug@gmail.com>
2018-06-25 19:28:09 +0300

    Task3

--- src/main/java/com/yurgen/mas/TaskMas2.java
+++ src/main/java/com/yurgen/mas/TaskMas2.java
@@ -6,14 +6,17 @@ public class TaskMas2 {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.print("Укажите любое число: ");
         int[] mas = {2, 4, 6, 8, 9,};
-        for (Integer a : mas) {
-            for (int i = 0; i < mas.length; i++) {
-                System.out.print("Введите єлемен: ");
-                mas[i] = in.nextInt();
-                System.out.println(String.format("Ты ввел %d * 2 = %d", mas[i], mas[i] * 2));
+        System.out.print("Укажите любое число: ");
+        for (int i = 0; i < mas.length; i++) {
+            mas[i] = in.nextInt();
+            if (mas[i] <= mas.length) {
+                System.out.println("Вы ввели число которое соответствуе заданому параметру: ");
+            } else {
+                System.out.println("Вы ввели число котрое не соответствует не одному параметру: ");
             }
         }
     }
 }
+
+
