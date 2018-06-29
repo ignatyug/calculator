ec73e1653189f34bc39fe3e796a6a4032780074f
Yura <ignatyug@gmail.com>
2018-06-30 01:18:01 +0300

    ArrayTask1

--- src/main/java/com/yurgen/array2/ArrayTask1.java
+++ src/main/java/com/yurgen/array2/ArrayTask1.java
@@ -2,17 +2,32 @@ package com.yurgen.array2;
 
 import java.util.Scanner;
 
+import static sun.misc.Version.print;
+
 public class ArrayTask1 {
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
         System.out.print("Введите длину массива: ");
+        Scanner in = new Scanner(System.in);
         int size = in.nextInt();
         char[] mas = new char[size];
+        launch(in, mas);
+        conclusion(mas);
+
+    }
+
+    private static void launch(Scanner in, char[] mas) {
         for (int i = 0; i < mas.length; i++) {
             System.out.print("Введите єлемен: ");
-            mas[i] = (char) in.nextInt();
+            mas[i] = in.next().charAt(0);
+        }
+    }
+
+    private static void conclusion(char[] mas) {
+        for (int i = 0; i < mas.length; i++) {
             System.out.println(i + " - " + mas[i] + ": " + (int) mas[i]);
         }
     }
 }
+
+
