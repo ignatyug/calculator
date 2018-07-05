9def3cb1cdebe6fc1cc601c86d6abf0c04f9e9b4
Yura <ignatyug@gmail.com>
2018-07-06 00:46:01 +0300

    ArrayTask1

--- src/main/java/com/yurgen/excel/ExcelTask1.java
+++ src/main/java/com/yurgen/excel/ExcelTask1.java
@@ -4,10 +4,23 @@ public class ExcelTask1 {
 
     public static void main(String[] args) {
         int[] number = {1, 2, 3, 4, 5, -1, -2, -3, -4, -5};
+        int sum1 = 0;
+        int sum2 = 0;
         for (int i = 0; i < number.length; i++) {
-            if (number[i] > 0)
-            System.out.print(number[i] + "\t");
+            if (number[i] < 0) {
+                sum1 += number[i];
+                System.out.print(sum1 + "\t");
+            }
+        }
+        System.out.print("\n");
+
+        for (int b = 0; b < number.length; b++) {
+            if (number[b] > 0) {
+                sum2 += number[b];
+                System.out.print(sum2 + "\t");
+            }
         }
     }
 }
+
 
