8231ff6398bd2e6fe4765f841e7528a27bbd9550
ignat <ignatyug@gmail.com>
2018-07-05 19:05:06 +0300

    ExcelTask1

--- src/main/java/com/yurgen/excel/ExcelTask1.java
+++ src/main/java/com/yurgen/excel/ExcelTask1.java
@@ -2,23 +2,12 @@ package com.yurgen.excel;
 
 public class ExcelTask1 {
 
-    public static void main (String[] args) {
-        int [][] number = {{36,1,51},{12,40,6},{7,18,9}};
-
-        for (int i=0; i<number.length; i++) {
-            for(int j=0; j<number[j].length; j++) {
-                System.out.print (number [i][j]+ "\t");
-            }
-            System.out.println();
+    public static void main(String[] args) {
+        int[] number = {1, 2, 3, 4, 5, -1, -2, -3, -4, -5};
+        for (int i = 0; i < number.length; i++) {
+            if (number[i] > 0)
+            System.out.print(number[i] + "\t");
         }
-        int summ=0;
-        for(int i=0; i<2; i++) {
-            for (int j=0; j<2; j++) {
-                if (number[i][j]>35) summ=number[i][j];
-
-            }
-        }
-        System.out.println(summ+" ");
-
     }
-    }
+}
+
