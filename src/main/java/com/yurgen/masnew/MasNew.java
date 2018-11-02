b4928a3695b33dd451eeae4f4d37772d6957da26
Yura <ignatyug@gmail.com>
2018-11-03 00:29:45 +0200

    MasNew

--- src/main/java/com/yurgen/masnew/MasNew.java
+++ src/main/java/com/yurgen/masnew/MasNew.java
@@ -3,15 +3,15 @@ package com.yurgen.masnew;
 public class MasNew {
     public static void main(String[] args) {
         String[] a = {"fifth", "twentieth",  "first", "tenth"};
-        String s = "first";
+        String s = new String ("fIrST");
         boolean b = false;
         for (int i = 0; i < a.length; i++) {
-            if (a[i] == s) {
+            if (a[i].equals(s)) {
                 System.out.println(i);
                 b = true;
             }
         }
-        if (b==false) {
+        if (!b) {
             System.out.println("no");
         }
     }
