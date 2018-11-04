c5a17810d168e2dc62910bddefe9c8f3f2b70c26
Yura <ignatyug@gmail.com>
2018-11-04 23:55:09 +0200

    MasNew

--- src/main/java/com/yurgen/masnew/MasNew.java
+++ src/main/java/com/yurgen/masnew/MasNew.java
@@ -1,12 +1,14 @@
 package com.yurgen.masnew;
 
+import java.util.Locale;
+
 public class MasNew {
     public static void main(String[] args) {
         String[] a = {"fifth", "twentieth",  "first", "tenth"};
-        String s = new String ("fIrST");
+        String s = new String ("FI");
         boolean b = false;
         for (int i = 0; i < a.length; i++) {
-            if (a[i].equals(s)) {
+            if (a[i].startsWith(s.toLowerCase())) {
                 System.out.println(i);
                 b = true;
             }
