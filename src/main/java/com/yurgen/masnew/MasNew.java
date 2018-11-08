706eb68de270a0ebf1f92d0cca60a19b09a52551
ignat <ignatyug@gmail.com>
2018-11-08 17:12:05 +0200

    MasNew

--- src/main/java/com/yurgen/masnew/MasNew.java
+++ src/main/java/com/yurgen/masnew/MasNew.java
@@ -5,10 +5,10 @@ import java.util.Locale;
 public class MasNew {
     public static void main(String[] args) {
         String[] a = {"fifth", "twentieth",  "first", "tenth"};
-        String s = new String ("FI");
+        String s = new String ("T");
         boolean b = false;
         for (int i = 0; i < a.length; i++) {
-            if (a[i].startsWith(s.toLowerCase())) {
+            if (a[i].contains(s.toLowerCase())) {
                 System.out.println(i);
                 b = true;
             }
