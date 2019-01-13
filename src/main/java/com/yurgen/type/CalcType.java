2e417266256764e85b07d761c8f5d534b747369e
Yura <ignatyug@gmail.com>
2019-01-14 01:55:52 +0200

    Type

--- src/main/java/com/yurgen/type/CalcType.java
+++ src/main/java/com/yurgen/type/CalcType.java
@@ -3,6 +3,15 @@ package com.yurgen.type;
 /**
  * Должно быть два поля для хранения целых чисел
  * Должен быть метод который складывает эти числа и возвращает результат
+ * https://vertex-academy.com/tutorials/ru/chto-takoe-metody-v-java/
  */
 public class CalcType {
+    public static void main(String[] args) {
+        double a = a.getNumber();
+        double b = b.getNumber();
+        char c = c.getOperation();
+        double j = j.calc(a,b,c);
+
+        System.out.println(j);
+    }
 }
